0fdfe81e072306224402c0d5011426d5108ac2cf
NazaTarrili <nazatarrili.proyectos@email.com>
2024-03-06 14:48:28 -0300

    Description:     Upgrade SEO
    On branch V2.0
    Your branch is up to date with 'origin/V2.0'.
        Changes to be committed:
            modified:   src/App.css
            modified:   src/components/Contact/Contact.css
            modified:   src/components/Contact/Contact.jsx
            modified:   src/components/Proyects/Proyects.css

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -4,16 +4,16 @@ import { contacts } from "../../data/contact"
 export const Contact = () => {
     return (
         <section className="Section Contact">
+            <h2 className='SectionTitle'>Contact</h2>
             <div className="ContactBar">
                 {
                     contacts.map(contact => (
-                            <div key={contact.id} className="IndividualContact">
-                                <a target="_blank" href={contact.url}>
-                                    <img className='ContactImage' src={contact.imageUrl} alt={contact.name} />
-                                </a>
-                            </div>
-                        )
-                    )
+                        <div key={contact.id} className="IndividualContact">
+                            <a target="_blank" href={contact.url}>
+                                <img className='ContactImage' src={contact.imageUrl} alt={contact.name} />
+                            </a>
+                        </div>
+                    ))
                 }
             </div>
         </section>
